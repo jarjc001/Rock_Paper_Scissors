00ab295ecc6c79a9651839037899e8161815a467
Christian Jarjat <jarjatc@gmail.com>
2023-02-14 13:47:02 +0000

    little start

---
+++ RockPaperScissors/src/main/java/org/example/RockPaperScissors.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class RockPaperScissors {
+
+    /**
+     *
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        //declare variables of the number of wins the user and computer has
+        //
+        int userWins, computerWins, ties;       /*change to Array*/
+
+        //declare the constant of the number of rounds
+        int ROUNDS;
+
+        //declare and initialise a Scanner object
+        //to read console inputs
+        Scanner myScanner = new Scanner(System.in);
+
+
+        //prompt user for the number of rounds they want to play
+        System.out.println("How many rounds do you want to play: ");
+        //Scans input
+        ROUNDS = myScanner.nextInt();
+
+
+
+
+
+    }
+}
